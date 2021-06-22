1ae855c5a7d5e894b2f1f48ad171a178424b375a
Louis Laugesen <louis.laugesen@gmail.com>
2021-06-22 17:40:41 +1000

    Get logmein copy pasta working without restriction
    Assumption check

--- apps/editing-toolkit/editing-toolkit-plugin/logmein/logmein.php
+++ apps/editing-toolkit/editing-toolkit-plugin/logmein/logmein.php
@@ -5,9 +5,11 @@
  * @package A8C\FSE
  */
 
-namespace A8C\FSE;
+namespace A8C\FSE\Logmein;
 
-add_action( 'init', __NAMESPACE__ . '\logmein_init' );
+/**
+ * Init
+ */
 function logmein_init() {
 	static $once = false;
 	if ( ! $once ) {
@@ -15,7 +17,11 @@ function logmein_init() {
 		add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\logmein_enqueue', 100 );
 	}
 }
+add_action( 'init', __NAMESPACE__ . '\logmein_init' );
 
+/**
+ * Enqueue
+ */
 function logmein_enqueue() {
 	$asset_file          = include __DIR__ . '/dist/logmein.asset.php';
 	$script_dependencies = $asset_file['dependencies'];
@@ -28,4 +34,29 @@ function logmein_enqueue() {
 		$version,
 		true
 	);
+}
+
+/**
+ * Api
+ */
+function logmein_api() {
+	register_rest_route(
+		'wpcom/v2',
+		'logmein',
+		array(
+			array(
+				'methods'  => \WP_REST_Server::READABLE,
+				'callback' => __NAMESPACE__ . '\\logmein_hosts',
+			),
+			'permission_callback' => '\is_user_logged_in',
+		)
+	);
+}
+
+/**
+ * Hosts
+ */
+function logmein_hosts() {
+	$hosts = apply_filters( 'wpcom_logmein_hosts', array() );
+	return rest_ensure_response( compact( 'hosts' ) );
 }
